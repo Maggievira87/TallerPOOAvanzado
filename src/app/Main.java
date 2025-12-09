5f16d95e65d52d96bcbd4cd5edb7e0015a578324
Maggievira87 <margarethjvr87@gmail.com>
2025-12-09 14:44:55 -0500

    Cambios Margareth

--- src/app/Main.java
+++ src/app/Main.java
@@ -9,14 +9,20 @@ public class Main {
 
         SistemaReservas sistema = new SistemaReservas();
 
-        Reserva r1 = new Reserva(14, "2026-02-15", "Maggie", 202453 );
-        Reserva r2 = new Reserva(24, "2026-03-16","Camilo",264684);
+        Reserva r1 = new Reserva(8, "2026-02-15", "Maggie", 202453 );
+        Reserva r2 = new Reserva(10, "2026-03-16","Camilo",264684);
+        Reserva r3 = new Reserva(12, "2026-03-22","Maria",254555);
+        Reserva r4 = new Reserva(16, "2026-05-08","Pablo",255556);
 
         sistema.agregarReserva(r1);
         sistema.agregarReserva(r2);
+        sistema.agregarReserva(r3);
+        sistema.agregarReserva(r4);
 
-        r2.setDuracionHoras(6);
         r1.setDuracionHoras(8);
+        r2.setDuracionHoras(10);
+        r3.setDuracionHoras(12);
+        r4.setDuracionHoras(16);
 
         sistema.listaDeReserva();
 
